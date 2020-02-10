dcdc6643e28dc677aa4a8bf425b037e808ce46cd
teorich <theo.foncham@gmail.com>
2020-02-10 23:11:56 +0100

    category screen added

---
+++ lib/categories_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+
+
+import './dummy_data.dart';
+import 'category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('DeliMeal'),
+              ),
+            body: GridView(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,childAspectRatio: 3/2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+
+          children: DUMMY_CATEGORIES.map((catData) => 
+            CategoryItem( 
+              catData.title, 
+            catData.color),
+            
+            ).toList(),
+
+
+          
+          ),
+      ),
+    );
+
+
+      
+  }
+}
